f5088dbde8149774e610b38e07b906b661da6514
Jack Wu <jackwu350@yahoo.com>
2018-02-05 21:46:24 -0800

    added passport module

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
 const app = express();
+passport.use(new GoogleStrategy());
 
 app.get("/", (req, res) => {
   res.send({ hello: "world" });

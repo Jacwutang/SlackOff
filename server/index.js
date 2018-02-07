8451da0ed78e3ea04aa7bd008a21f4e41f00c498
Jack Wu <jackwu350@yahoo.com>
2018-02-06 18:49:09 -0800

    phase 1 of authentication

--- server/index.js
+++ server/index.js
@@ -10,19 +10,33 @@ passport.use(
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
-  }, (accessToken) => {
-    console.log(accessToken);
-  })
+  }, (accessToken, refreshToken,profile,done) => {
+    console.log('accessToken', accessToken);
+    console.log('refreshToken',refreshToken);
+    console.log('profile',profile);
+    }
+  )
 );
 
 // client-id 929951212884-s07g24p8urh1fc5gpmin7mk58jphdkqb.apps.googleusercontent.com  public token
 // client-secret a3X0WSYEdkVs9oSKXegN84fo private token
 
 
-app.get("/", (req, res) => {
-  res.send({ hello: "world" });
-});
+// app.get("/", (req, res) => {
+//   res.send({ hello: "world" });
+// });
 
-const PORT = process.env.PORT || 5000;
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile','email']
+  })
+);
+
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+
+const PORT = process.env.PORT ||  5000;
 
 app.listen(PORT);

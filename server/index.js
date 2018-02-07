f45285362c492afe6fbfeb41028b15a60381dd87
Jack Wu <jackwu350@yahoo.com>
2018-02-06 21:54:06 -0800

    refactored server

--- server/index.js
+++ server/index.js
@@ -1,22 +1,12 @@
 const express = require("express");
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
 
 
 const app = express();
-passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
-  }, (accessToken, refreshToken,profile,done) => {
-    console.log('accessToken', accessToken);
-    console.log('refreshToken',refreshToken);
-    console.log('profile',profile);
-    }
-  )
-);
+require('./routes/authRoutes')(app);
+
+
+
 
 // client-id 929951212884-s07g24p8urh1fc5gpmin7mk58jphdkqb.apps.googleusercontent.com  public token
 // client-secret a3X0WSYEdkVs9oSKXegN84fo private token
@@ -26,15 +16,7 @@ passport.use(
 //   res.send({ hello: "world" });
 // });
 
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile','email']
-  })
-);
 
-
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 
 const PORT = process.env.PORT ||  5000;
---
+++ server/routes/authRoutes.js
@@ -0,0 +1,13 @@
+const passport = require('passport');
+
+module.exports = (app) => {
+
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile','email']
+    })
+  );
+  app.get('/auth/google/callback', passport.authenticate('google'));
+
+};

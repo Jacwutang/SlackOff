35cc967663ae1edeeec97d7b10e3b0f46c87d7f3
Jack Wu <jackwu350@yahoo.com>
2018-02-06 16:00:16 -0800

    setup passport use params

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 
 const app = express();
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+  }, (accessToken) => {
+    console.log(accessToken);
+  })
+);
+
+// client-id 929951212884-s07g24p8urh1fc5gpmin7mk58jphdkqb.apps.googleusercontent.com  public token
+// client-secret a3X0WSYEdkVs9oSKXegN84fo private token
+
 
 app.get("/", (req, res) => {
   res.send({ hello: "world" });

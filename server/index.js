7d12cd8ec2f81d9c4c44f9175ca00eea53a1821b
Jack Wu <jackwu350@yahoo.com>
2018-02-09 23:08:58 -0800

    added mongoose module

--- server/index.js
+++ server/index.js
@@ -1,22 +1,11 @@
 const express = require("express");
+const mongoose = require('mongoose');
 require('./services/passport');
+const keys = require('./config/keys');
 
-
+mongoose.connect(keys.mongoURI);
 const app = express();
 require('./routes/authRoutes')(app);
-
-
-
-
-// client-id 929951212884-s07g24p8urh1fc5gpmin7mk58jphdkqb.apps.googleusercontent.com  public token
-// client-secret a3X0WSYEdkVs9oSKXegN84fo private token
-
-
-// app.get("/", (req, res) => {
-//   res.send({ hello: "world" });
-// });
-
-
 
 
 const PORT = process.env.PORT ||  5000;

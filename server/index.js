0f75d29b9f8272e3a6f071c01c6cf5c67f941d31
Jack Wu <jackwu350@yahoo.com>
2018-02-13 20:32:58 -0800

    configured react-server and express server concurrently

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');

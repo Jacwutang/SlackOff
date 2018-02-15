684815cdd4170ec1d5f7e9e9cf259eef2da0698f
Jack Wu <jackwu350@yahoo.com>
2018-02-14 18:06:07 -0800

    installed materialize-css

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
-import Landing from './landing';
+import Landing from './Landing';
 
 const App = () => {
   return(
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,8 +1,10 @@
 //DATA LAYER
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
+
 
 import App from './components/App';
 import reducers from './reducers';

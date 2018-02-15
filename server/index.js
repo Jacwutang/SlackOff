54c01e03736649d8e9936bd0d4a33559e3199b03
Jack Wu <jackwu350@yahoo.com>
2018-02-14 21:48:48 -0800

    .

--- server/index.js
+++ server/index.js
@@ -29,22 +29,22 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // load route handlers
-require('./routes/authRoutes')(app);
+// require('./routes/authRoutes')(app);
 
 
 
-if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production assets
-  // Like main.js or main.css files
-  app.use(express.static('client/build'));
-  // Express will serve up the index.html file
-  // if it doesn't recognize the route
-  const path = require('path');
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   // Express will serve up production assets
+//   // Like main.js or main.css files
+//   app.use(express.static('client/build'));
+//   // Express will serve up the index.html file
+//   // if it doesn't recognize the route
+//   const path = require('path');
+//
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 
 const PORT = process.env.PORT ||  5000;

04d8964acf28179e8e7f0a8c2e2bb228cf585654
Jack Wu <jackwu350@yahoo.com>
2018-02-14 21:57:26 -0800

    .

--- server/index.js
+++ server/index.js
@@ -28,23 +28,23 @@ app.use(passport.initialize());
 //passport.session middleware is a Passport Strategy which will load the user object onto req.user or req.session? if a serialised user object was found in the server.
 app.use(passport.session());
 
-// load route handlers
-// require('./routes/authRoutes')(app);
+load route handlers
+require('./routes/authRoutes')(app);
 
 
 
-// if (process.env.NODE_ENV === 'production') {
-//   // Express will serve up production assets
-//   // Like main.js or main.css files
-//   app.use(express.static('client/build'));
-//   // Express will serve up the index.html file
-//   // if it doesn't recognize the route
-//   const path = require('path');
-//
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // Like main.js or main.css files
+  app.use(express.static('server/client/build'));
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 
 const PORT = process.env.PORT ||  5000;

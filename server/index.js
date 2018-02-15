80173a97fa29b01072f54ed8405c9c15fffa0547
Jack Wu <jackwu350@yahoo.com>
2018-02-14 21:46:27 -0800

    added production handlers

--- server/index.js
+++ server/index.js
@@ -32,6 +32,21 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 
 
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // Like main.js or main.css files
+  app.use(express.static('client/build'));
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
 const PORT = process.env.PORT ||  5000;
 
 app.listen(PORT);

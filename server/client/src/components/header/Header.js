bde75a4c7996b5b065b95580f1958bd23c28123f
Jack Wu <jackwu350@yahoo.com>
2018-02-19 17:41:28 -0800

    implemented conditional redirect of logo

--- server/client/src/components/header/Header.js
+++ server/client/src/components/header/Header.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './navbar.css';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   renderContent(){
@@ -33,10 +34,13 @@ class Header extends Component {
     return(
       <nav>
         <div className="nav-wrapper">
-          <a href="" className="brand-logo">
+          <Link
+            to={this.props.auth ? '/channels' : '/' }
+            className="brand-logo"
+          >
            <img src={require("../../images/Slack_Mark_Web.png")} alt="logo" width={75}  />
-          <span className="logo-span">SlackOff</span>
-          </a>
+           <span className="logo-span">SlackOff</span>
+          </Link>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>
               <a href="">

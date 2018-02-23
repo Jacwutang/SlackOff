809994975b564c5cbe447c2f332fe17a7c6c7288
Jack Wu <jackwu350@yahoo.com>
2018-02-23 10:48:38 -0800

    .

--- server/client/src/components/channel/dashboard/index.js
+++ server/client/src/components/channel/dashboard/index.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
 import './index.css';
+import ChannelDisplay from './'
+
 
 class DashBoard extends Component {
   render(){
     return(
-      <div className="col s2">
-        Side column
-      </div>
+      <section className="col s2">
+        <div className="workspace-div"> Workspace </div>
+        <section className="dashboard-message">
+          <div> Channels
+
+          </div>
+          <div> Direct Messages </div>
+        </section>
+      </section>
     );
   }
 

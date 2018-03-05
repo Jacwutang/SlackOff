79051344b9fc7e2ecf4ed812939d1211a6d9c640
Jack Wu <jackwu350@yahoo.com>
2018-03-05 14:43:02 -0800

    created input field for submitting messages

--- server/client/src/components/channel/dashboard/channelDisplay/channelDisplay.js
+++ server/client/src/components/channel/dashboard/channelDisplay/channelDisplay.js
@@ -22,7 +22,7 @@ class ChannelDisplay extends Component {
   }
 
   componentDidMount(){
-    console.log(this.props);
+    // console.log(this.props);
   }
   componentWillReceiveProps(nextProps){
     if(this.props === nextProps)
--- server/client/src/components/channel/message/Message.js
+++ server/client/src/components/channel/message/Message.js
@@ -4,7 +4,41 @@ import {connect} from 'react-redux';
 import './message.css';
 
 class Message extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      input: ""
+    }
+
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  componentDidMount(){
+
+  }
+
+  handleInput(field){
+
+
+    return(
+      (e) => this.setState({[field]: e.target.value})
+    )
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    window.alert("SUBMITEED");
+
+
+  }
+
+
   render(){
+    console.log(this.state.input);
+
     return(
 
       <div className="col s10">
@@ -20,7 +54,14 @@ class Message extends Component {
         </div>
 
         <div className="bottom-row">
-          Bottom Row
+        <form onSubmit={this.handleSubmit} className="msg-input">
+          <input
+          value={this.state.input}
+          onChange={this.handleInput('input')} />
+          <button> Submit NOW </button>
+        </form>
+
+
         </div>
       </div>
 
@@ -37,7 +78,7 @@ function mapStateToProps(state){
     {
 
     }
-  )
+  );
 };
 
 

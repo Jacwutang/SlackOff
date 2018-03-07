e6794805bb7023a2d68d8c1cf7eabf54aec112a0
Jack Wu <jackwu350@yahoo.com>
2018-03-06 16:46:00 -0800

    wrapped li with Link

--- server/client/src/components/channel/dashboard/channelDisplay/channelIndexItem.js
+++ server/client/src/components/channel/dashboard/channelDisplay/channelIndexItem.js
@@ -30,15 +30,19 @@ class ChannelIndexItem extends Component{
     const { channel } = this.props;
 
     return(
+      <Link to={`/messages/channel/${channel._id}`}>
+
       <li
       className="channel-index-item"
       tabindex="1"
 
       >
-        <Link to={`/messages/channel/${channel._id}`}>
+
         {channel.name}
-        </Link>
+
       </li>
+
+      </Link>
     );
   }
 

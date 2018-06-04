b810d452b9c7c4cc1952fbc30a86980a16aaf362
Jack Wu <jackwu350@yahoo.com>
2018-06-04 12:14:14 -0700

    remove unused models

--- server/models/DirectMessageChannel.js
+++
@@ -1,7 +0,0 @@
-// const mongoose = require('mongoose');
-// const { Schema } = mongoose;
-//
-// const directMessageChannelSchema = new Schema({
-//     members: [{ type: Schema.Types.ObjectId, ref: 'User'}],
-//
-//   });

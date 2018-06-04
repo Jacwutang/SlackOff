4c2952012f11ccbfbe0db174d5f8cc4f8e4b5628
Jack Wu <jackwu350@yahoo.com>
2018-06-04 12:16:56 -0700

    redirect to current portfolio

--- server/client/src/components/footer/Footer.js
+++ server/client/src/components/footer/Footer.js
@@ -1,32 +1,46 @@
-import React from 'react';
-import 'assets/css/Footer/footer.css';
+import React from "react";
+import "assets/css/Footer/footer.css";
 
 export default () => {
-  return(
-
+  return (
     <div className="container">
-    <nav className="nav-footer">
-      <div className="div-footer">
-        <h4 className="h4-footer"> You're in Good Hands </h4>
+      <nav className="nav-footer">
+        <div className="div-footer">
+          <h4 className="h4-footer"> You're in Good Hands </h4>
 
-        <p className="p-footer-description"> Join millions of people around the world and SlackOff</p>
+          <p className="p-footer-description">
+            {" "}
+            Join millions of people around the world and SlackOff
+          </p>
 
-        <p className="p-footer">
-          <a target="_blank" rel="noopener noreferrer" href="https://github.com/Jacwutang">
-            <img src={require("../../images/GitHub.png")} alt="github" />
-          </a>
-          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jacwutang/">
-            <img src={require("../../images/In.png")} alt="linkedin" />
-          </a>
-          <a target="_blank" rel="noopener noreferrer" href="https://jacwutang.github.io/Portfolio/">
-          <img src={require("../../images/briefcase.png")} alt="portfolio" />
-          </a>
-        </p>
-
-      </div>
-    </nav>
-
+          <p className="p-footer">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/Jacwutang"
+            >
+              <img src={require("../../images/GitHub.png")} alt="github" />
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/jacwutang/"
+            >
+              <img src={require("../../images/In.png")} alt="linkedin" />
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="http://jackwu.rocks/"
+            >
+              <img
+                src={require("../../images/briefcase.png")}
+                alt="portfolio"
+              />
+            </a>
+          </p>
+        </div>
+      </nav>
     </div>
   );
-
 };
